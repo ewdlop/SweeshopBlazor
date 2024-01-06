ddd335edf67b7a171b4359b3e9d76be3b13c03ce
ewdlop <ray810815@gmail.com>
2024-01-06 15:27:37 +0000

    Deploying to gh-pages from  @ f61709acf6deb8890f4b94f174978d944f6980ac 🚀

---
+++ _content/Soenneker.Blazor.Masonry/masonry.js
@@ -0,0 +1,7 @@
+﻿window.initMasonry = (containerSelector, itemSelector, percentPosition, transitionDuration) => {
+    var masonry = new Masonry(containerSelector, {
+        percentPosition: percentPosition,
+        transitionDuration: transitionDuration,
+        itemSelector: itemSelector
+    });
+};
